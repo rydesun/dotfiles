e4aa76ac6e034d8137ff1057b75d1828ac1ba2f2
rydesun <rydesun@gmail.com>
2021-08-25 03:03:30 +0800

    Rewrite zsh config

--- .config/zsh/hostrc.d/funcs.zsh
+++
@@ -1,8 +0,0 @@
-# Arch Linux
-alias pmq='pacman -Qs'
-alias pms='pacman -Ss'
-pmi() { pacman -Qi $1 2>/dev/null || pacman -Sii $1 }
-pmo() { pacman -Qoq $1 2>/dev/null || pkgfile -i $1 }
-pml() { (pacman -Qlq $1 2>/dev/null || pkgfile -lq $1) | sed '/\/$/d' }
-pmb() { (pacman -Qlq $1 2>/dev/null || pkgfile -lq $1) | awk -F/ '/\/usr\/bin\/.+[^/]$/{print $NF}' }
-pmd() { (pacman -Qlq $1 2>/dev/null || pkgfile -lq $1) | grep -e '\.service$' -e '\.socket$' }
